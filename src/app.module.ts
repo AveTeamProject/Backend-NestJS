b5139950e701fbc811bbdb90ce3817242fe6850f
Hau. Le Cong <leconghau095@gmail.com>
2024-05-01 21:39:46 +0700

    add configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import configuration from './config/configuration'
+import { validate } from 'env.validation'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${process.cwd()}/.env.${process.env.NODE_ENV}`],
+      isGlobal: true,
+      load: [configuration],
+      validate: validate
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
